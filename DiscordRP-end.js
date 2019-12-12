eb3c3d5f6976010afd67addb97138242d3ea6c1a
DELUUXE <info@deluuxe.nl>
2019-12-13 00:17:08 +0100

    [update] discord rich presence. states: starting, stopping and editing settings

---
+++ DiscordRP-end.js
@@ -0,0 +1,27 @@
+const RPC = require('discord-rpc')
+const client = new RPC.Client({ transport: 'ipc' })
+
+client.on('ready', () => {
+    client.setActivity({
+        state: 'By DELUUXE',
+        details: 'integration for Razer Chroma (unofficial)',
+        state: 'Closing...',
+        largeImageKey: 'discordchroma',
+        largeImageDetails: "DiscordChroma",
+        instance: true,
+    })
+
+    client.on('error', function (error) {
+        console.log(error)
+        client.clearActivity()
+        client.destroy()
+        process.exit(0)
+    })
+})
+
+process.on('exit', () => {
+    client.clearActivity()
+    client.destroy()
+})
+
+client.login({ clientId: '653623540649820181' })
